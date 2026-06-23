03fd736a694758eeaa5e5a7ea2a683304a51265d
jaforahmed1000ss-lgtm <jaforahmed1000ss@gmail.com>
2026-06-23 10:35:11 +0600

    fix: remove Firebase from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:media_kit/media_kit.dart';
-import 'firebase_options.dart';
-import 'screens/splash_screen.dart';
+  import 'package:media_kit/media_kit.dart';
+  import 'screens/splash_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  MediaKit.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const CricZTVApp());
-}
-
-class CricZTVApp extends StatelessWidget {
-  const CricZTVApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'CricZ TV',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color(0xFF000000),
-        colorScheme: const ColorScheme.dark(
-          primary: Color(0xFF00D2FF),
-        ),
-      ),
-      home: const SplashScreen(),
-    );
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    MediaKit.ensureInitialized();
+    runApp(const CricZTVApp());
   }
-}
+
+  class CricZTVApp extends StatelessWidget {
+    const CricZTVApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'CricZ TV',
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: const Color(0xFF000000),
+          colorScheme: const ColorScheme.dark(
+            primary: Color(0xFF00D2FF),
+          ),
+        ),
+        home: const SplashScreen(),
+      );
+    }
+  }
+  
